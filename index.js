e56df15eef2c08e13005fe0129d3d3ecdda2a1e8
Replit user <>
2023-10-08 01:41:12 +0300

    first commit

--- index.js
+++ index.js
@@ -20,9 +20,19 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+
+app.get("/api/whoami",(req,res)=>{
+    const ipAddress=req.ip;
+    const languageHeader=req.headers["accept-language"];
+    const softwareHeader=req.headers["user-agent"];
+
+    res.json({
+        ipaddress:ipAddress,
+        language:languageHeader,
+        software:softwareHeader
+    });
+
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
